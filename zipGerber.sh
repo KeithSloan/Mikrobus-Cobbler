49b9834b71ca779947a839ad02f3016ecc684b46
Keith Sloan <keith@sloan-home.co.uk>
2015-12-22 22:13:17 +0000

    Add PCB with Drill Holes as this maybe easier to make Fritzing part

--- zipGerber.sh
+++ zipGerber.sh
@@ -2,3 +2,7 @@
 name1="Mikrobus-Cobbler"
 echo $name1
 zip $name1.zip $name1.cmp $name1.drd $name1.dri $name1.gpi $name1.plc $name1.pls $name1.sol $name1.stc $name1.sts
+name2="Mikrobus-Cobbler-Drill-Holes"
+echo $name2
+zip $name2.zip $name2.cmp $name2.drd $name2.dri $name2.gpi $name2.plc $name2.pls $name2.sol $name2.stc $name2.sts
+
