fb032216c99af7d984a1b72d4cb698c723f0427f
Keith Sloan <keith@sloan-home.co.uk>
2015-12-28 07:04:27 +0000

    Provide ITeadStudio Gerber files

--- zipGerber.sh
+++ zipGerber.sh
@@ -2,6 +2,9 @@
 name1="Mikrobus-Cobbler"
 echo $name1
 zip $name1.zip $name1.cmp $name1.drd $name1.dri $name1.gpi $name1.plc $name1.pls $name1.sol $name1.stc $name1.sts
+echo "ITeadStudio"
+zip $name1"ITeadStudio.zip" $name1.TXT $name1.GML $name1.do $name1.GBP $name1.GTP $name1.GBS $name1.GTS $name1.GBO $name1.GTO $name1.GBL $name1.GTL $name1" .GMx"
+
 name2="Mikrobus-Cobbler-Drill-Holes"
 echo $name2
 zip $name2.zip $name2.cmp $name2.drd $name2.dri $name2.gpi $name2.plc $name2.pls $name2.sol $name2.stc $name2.sts
